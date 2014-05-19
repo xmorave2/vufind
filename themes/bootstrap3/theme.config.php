2fa03a6bce2e9119f20c6e208907ba6e2d7372c7
Chris Hallberg <crhallberg@gmail.com>
2014-05-19 09:56:29 -0400

    It's happening.

---
+++ themes/bootstrap3/theme.config.php
@@ -0,0 +1,32 @@
+<?php
+return array(
+    'extends' => 'root',
+    'css' => array(
+        'bootstrap.min.css',
+        'font-awesome.min.css',
+        'bootstrap-accessibility.css',
+        'bootstrap-custom.css',
+        'print.css:print',
+        'slider.css',
+    ),
+    'js' => array(
+        'core/jquery.min.js',
+        'core/bootstrap.min.js',
+        'core/bootstrap-accessibility.min.js',
+        'common.js',
+        'lightbox.js',
+        'rc4.js'
+    ),
+    'favicon' => 'vufind-favicon.ico',
+    'helpers' => array(
+        'factories' => array(
+            'flashmessages' => 'VuFind\View\Helper\Bootstrap3\Factory::getFlashmessages',
+            'layoutclass' => 'VuFind\View\Helper\Bootstrap3\Factory::getLayoutClass',
+        ),
+        'invokables' => array(
+            'highlight' => 'VuFind\View\Helper\Bootstrap3\Highlight',
+            'search' => 'VuFind\View\Helper\Bootstrap3\Search',
+            'vudl' => 'VuDL\View\Helper\Bootstrap3\VuDL',
+        )
+    )
+);
