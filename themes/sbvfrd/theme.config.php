cda6268ef1ef29ae5fa301d1cb5a37c2e23c9a7c
Nicolas Karrer <nkarrer@snowflake.ch>
2014-06-26 17:14:38 +0200

    Added theme-specific template for record detail view Added theme-specific templates for record detail view sidebar

--- themes/sbvfrd/theme.config.php
+++ themes/sbvfrd/theme.config.php
@@ -4,5 +4,15 @@ return array(
 
   'css'	=> array(
     'sbvfrd.css',
-  )
+  ),
+
+  'helpers' => array(
+     'factories' => array(
+         'record'                        => 'Swissbib\View\Helper\Swissbib\Factory::getRecordHelper',
+         'flashmessages'                 => 'Swissbib\View\Helper\Swissbib\Factory::getFlashMessages',
+         'citation'                      => 'Swissbib\View\Helper\Swissbib\Factory::getCitation',
+         'recordlink'                    => 'Swissbib\View\Helper\Swissbib\Factory::getRecordLink',
+         'getextendedlastsearchlink'     => 'Swissbib\View\Helper\Swissbib\Factory::getExtendedLastSearchLink'
+     )
+   )
 );
