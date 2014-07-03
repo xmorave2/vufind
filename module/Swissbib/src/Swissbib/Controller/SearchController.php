b98ae2a77f6d4d0a77933200cc3b0166073a26d2
mmaechler <mmaechler@snowflake.ch>
2014-07-03 17:02:12 +0200

    Removed unused tab features

--- module/Swissbib/src/Swissbib/Controller/SearchController.php
+++ module/Swissbib/src/Swissbib/Controller/SearchController.php
@@ -5,7 +5,6 @@ use Zend\Config\Config;
 use Zend\Http\PhpEnvironment\Response;
 use Zend\Session\Container as SessionContainer;
 use Zend\View\Model\ViewModel;
-use Zend\View\Resolver\ResolverInterface;
 
 use VuFind\Controller\SearchController as VuFindSearchController;
 use VuFind\Search\Results\PluginManager as VuFindSearchResultsPluginManager;
@@ -27,47 +26,14 @@ class SearchController extends VuFindSearchController
 
 
     /**
-     * (Default Action) Get model for home view
-     *
-     * @return    \Zend\View\Model\ViewModel
-     */
-    public function homeAction()
-    {
-        $allTabsConfig      = $this->getThemeTabsConfig();
-        $activeTabKey       = $this->getActiveTab();
-        $resultsFacetConfig = $this->getFacetConfig();
-        $activeTabConfig    = $allTabsConfig[$activeTabKey];
-
-        // Set default target
-        $this->searchClassId = $activeTabConfig['searchClassId'];
-
-        $homeView = parent::homeAction();
-
-        $this->layout()->setVariable('searchClassId', $this->searchClassId);
-        $this->layout()->setVariable('pageClass', 'template_home');
-
-        return $homeView;
-    }
-
-
-
-    /**
      * Get model for general results view (all tabs, content of active tab only)
      *
      * @return \Zend\View\Model\ViewModel
      */
     public function resultsAction()
     {
-        $allTabsConfig      = $this->getThemeTabsConfig();
-        $activeTabKey       = $this->getActiveTab();
         $resultsFacetConfig = $this->getFacetConfig();
-        $activeTabConfig    = $allTabsConfig[$activeTabKey];
-
-        // Set default target
-        $this->searchClassId = $activeTabConfig['searchClassId'];
-
         //do not remember FRBR searches because we ant to jump back to the original search
-
         $type = $this->params()->fromQuery('type');
 
         if (!empty($type) && $type == "FRBR") {
@@ -80,14 +46,7 @@ class SearchController extends VuFindSearchController
             return $resultViewModel;
         }
 
-        $allTabsConfig[$activeTabKey]['active'] = true;
-        $allTabsConfig[$activeTabKey]['count']  = $resultViewModel->results->getResultTotal();
-
         $this->layout()->setVariable('resultViewParams', $resultViewModel->getVariable('params'));
-
-        $resultViewModel->setVariable('allTabsConfig', $allTabsConfig);
-        $resultViewModel->setVariable('activeTabKey', $activeTabKey);
-        $resultViewModel->setVariable('activeTabConfig', $activeTabConfig);
         $resultViewModel->setVariable('facetsConfig', $resultsFacetConfig);
 
         return $resultViewModel;
@@ -126,90 +85,6 @@ class SearchController extends VuFindSearchController
         }
 
         return $viewModel;
-    }
-
-
-
-    /**
-     * Find active tab
-     *
-     * @return    String
-     */
-    protected function getActiveTab()
-    {
-        if ($this->forceTabKey) {
-            $activeTabKey = $this->forceTabKey;
-        } else {
-            $activeTabKey   = trim(strtolower($this->params()->fromRoute('tab')));
-            $allTabsConfig  = $this->getThemeTabsConfig();
-
-            if (empty($activeTabKey) || !isset($allTabsConfig[$activeTabKey])) {
-                $activeTabKey = key($allTabsConfig);
-            }
-        }
-
-        return $activeTabKey;
-    }
-
-
-
-    /**
-     * Get template for tab
-     * A tab template always contains a tab-key postfox
-     *
-     * @example
-     * TabKey: foobar
-     * Base Template: path/to/base-template.phtml
-     * Tab Template:  path/to/base-template.foobar.phtml
-     *
-     * Returns the path to the tab template if available. Else return base template
-     *
-     * @param    String $tab
-     * @param    String $baseTemplate
-     *
-     * @return    String
-     */
-    protected function getTabTemplate($tab, $baseTemplate)
-    {
-        /** @var ResolverInterface $resolver */
-        $resolver   = $this->serviceLocator->get('Zend\View\Renderer\PhpRenderer')->resolver();
-        $pathInfo   = pathInfo($baseTemplate);
-        $tab        = strtolower($tab);
-        $customTemplate = $pathInfo['dirname'] .
-            '/' . $pathInfo['basename'] .
-            '.' . $tab .
-            (isset($pathInfo['extension']) ? '.' . $pathInfo['extension'] : '');
-
-        return $resolver->resolve($customTemplate) !== false ? $customTemplate : $baseTemplate;
-    }
-
-
-
-    /**
-     * Get all configuration for theme tabs
-     *
-     * @return    Array[]
-     */
-    protected function getThemeTabsConfig()
-    {
-        return $this->getServiceLocator()->get('Swissbib\Theme\Theme')->getThemeTabsConfig();
-    }
-
-
-
-    /**
-     * Get base view model
-     * Inject search class id into layout
-     *
-     * @param    Array|null $params
-     *
-     * @return    ViewModel
-     */
-    protected function createViewModel($params = null)
-    {
-        $this->layout()->setVariable('searchClassId', $this->searchClassId);
-
-        return parent::createViewModel($params);
     }
 
 
