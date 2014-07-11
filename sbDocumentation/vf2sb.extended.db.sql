b930b278c78ff4db9ec85765a29bbccead16ff2c
Nicolas Karrer <nkarrer@snowflake.ch>
2014-07-11 14:30:28 +0200

    extend user table for default-sorting-preference

--- sbDocumentation/vf2sb.extended.db.sql
+++ sbDocumentation/vf2sb.extended.db.sql
@@ -4,4 +4,5 @@
 ALTER TABLE `user` ADD `favorite_institutions` TEXT NOT NULL;
 ALTER TABLE `user` ADD `language` VARCHAR( 2 ) NOT NULL DEFAULT '';
 ALTER TABLE `user` ADD `max_hits` SMALLINT NOT NULL DEFAULT '0';
+ALTER TABLE `user` ADD `default_sorting` VARCHAR(255) NOT NULL DEFAULT '';
 alter table user modify username VARCHAR(200);
