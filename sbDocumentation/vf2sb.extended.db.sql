26ca0d61db0bb01a810c7657d5a76d6285f07181
Guenter Hipler <guenter.hipler@unibas.ch>
2014-07-15 10:39:59 +0200

    Documentation: Modifications on VuFind DBMS schema compare: https://github.com/swissbib/vufind/issues/24

--- sbDocumentation/vf2sb.extended.db.sql
+++ sbDocumentation/vf2sb.extended.db.sql
@@ -6,3 +6,11 @@ ALTER TABLE `user` ADD `language` VARCHAR( 2 ) NOT NULL DEFAULT '';
 ALTER TABLE `user` ADD `max_hits` SMALLINT NOT NULL DEFAULT '0';
 ALTER TABLE `user` ADD `default_sort` VARCHAR(255) NOT NULL DEFAULT '';
 alter table user modify username VARCHAR(200);
+
+#You have to do it by yourself because it's part of VuFind DBMS schema (no swissbib extension)
+#ALTER TABLE `user` ADD `verify_hash` varchar(42) NOT NULL DEFAULT '';
+#`sb_nickname` text NOT NULL, seems to be a column created in the beginnings of our development
+#delete it with
+#ALTER TABLE `user` DROP COLUMN `sb_nickname`;
+
+
