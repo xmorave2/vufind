c9c34006294da2e2dd3606e64807dbecb8c5f48e
mmaechler <mmaechler@snowflake.ch>
2015-01-07 14:40:22 +0100

    #66 Implented IE8 support part I

--- themes/sbvfrd/js/swissbib/OffCanvas.js
+++ themes/sbvfrd/js/swissbib/OffCanvas.js
@@ -12,26 +12,26 @@ var OffCanvas = {
    * Initialize Off-Canvas handling
    */
   init: function () {
-    this.initOffCanvas();
+    OffCanvas.initOffCanvas();
   },
 
   /**
    *
    */
   initOffCanvas: function () {
-    this.sidebar = $(".sidebar");
-    this.button = $("button#sidebar-offcanvas-trigger");
-    this.icon = $("button#sidebar-offcanvas-trigger i");
+    OffCanvas.sidebar = $(".sidebar");
+    OffCanvas.button = $("button#sidebar-offcanvas-trigger");
+    OffCanvas.icon = $("button#sidebar-offcanvas-trigger i");
 
-    if (this.sidebar.length > 0) {
-      this.body = $("body");
-      this.footer = $("footer");
-      this.main = $(".main");
-      this.header = $("header");
+    if (OffCanvas.sidebar.length > 0) {
+      OffCanvas.body = $("body");
+      OffCanvas.footer = $("footer");
+      OffCanvas.main = $(".main");
+      OffCanvas.header = $("header");
 
-      this.button.click(this.toggleOffCanvas.bind(this));
+      OffCanvas.button.click(OffCanvas.toggleOffCanvas);
     } else {
-      this.button.hide();
+      OffCanvas.button.hide();
     }
   },
 
@@ -39,28 +39,28 @@ var OffCanvas = {
    * Toggle Off Canvas
    */
   toggleOffCanvas: function () {
-    this.enableTransition();
+    OffCanvas.enableTransition();
 
-    if (this.body.hasClass("offcanvas-active")) {
-      this.body.removeClass("offcanvas-active");
-      this.button.removeClass("offcanvas-active");
-      this.sidebar.removeClass("offcanvas-active");
-      this.sidebar.css('height', '');
-      this.sidebar.css('overflow-y', '');
-      this.body.css('height', '');
-      this.body.css('overflow-y', '');
-      this.icon.removeClass("fa fa-caret-right");
-      this.icon.addClass("fa fa-caret-left");
+    if (OffCanvas.body.hasClass("offcanvas-active")) {
+      OffCanvas.body.removeClass("offcanvas-active");
+      OffCanvas.button.removeClass("offcanvas-active");
+      OffCanvas.sidebar.removeClass("offcanvas-active");
+      OffCanvas.sidebar.css('height', '');
+      OffCanvas.sidebar.css('overflow-y', '');
+      OffCanvas.body.css('height', '');
+      OffCanvas.body.css('overflow-y', '');
+      OffCanvas.icon.removeClass("fa fa-caret-right");
+      OffCanvas.icon.addClass("fa fa-caret-left");
     } else {
-      this.body.addClass("offcanvas-active");
-      this.button.addClass("offcanvas-active");
-      this.sidebar.addClass("offcanvas-active");
-      this.sidebar.css('height', window.innerHeight);
-      this.sidebar.css('overflow-y', 'scroll');
-      this.body.css('height', window.innerHeight);
-      this.body.css('overflow-y', 'hidden');
-      this.icon.removeClass("fa fa-caret-left");
-      this.icon.addClass("fa fa-caret-right");
+      OffCanvas.body.addClass("offcanvas-active");
+      OffCanvas.button.addClass("offcanvas-active");
+      OffCanvas.sidebar.addClass("offcanvas-active");
+      OffCanvas.sidebar.css('height', window.innerHeight);
+      OffCanvas.sidebar.css('overflow-y', 'scroll');
+      OffCanvas.body.css('height', window.innerHeight);
+      OffCanvas.body.css('overflow-y', 'hidden');
+      OffCanvas.icon.removeClass("fa fa-caret-left");
+      OffCanvas.icon.addClass("fa fa-caret-right");
     }
   },
 
@@ -68,11 +68,11 @@ var OffCanvas = {
    * Workaround to prevent transition on orientation change
    */
   enableTransition: function() {
-    this.sidebar.addClass('transition');
+    OffCanvas.sidebar.addClass('transition');
     setTimeout(function() {
       $('.sidebar').removeClass('transition');
     }, 400);
   }
 };
 
-$(OffCanvas.init.bind(OffCanvas));
+$(OffCanvas.init);
--- themes/sbvfrd/theme.config.php
+++ themes/sbvfrd/theme.config.php
@@ -1,80 +1,81 @@
 <?php
 return array(
-    'extends' => 'bootstrap3',
+  'extends' => 'bootstrap3',
 
-    'less' => array(
-        'active' => false,
-        'compiled.less'
-    ),
+  'less'    => array(
+    'active' => false,
+    'compiled.less'
+  ),
 
-    'js' => array(
-        'jquery/ui/jquery-ui.min.js',
-//
-        'lib/jstorage.min.js', //used for favorites - there is still some amount of JS code inline of the page -> Todo: Refactoring in upcoming Sprints
-        //wird in swissbib/AdvancedSearch.js verwendet
-        'lib/handlebars.js',
-        'lib/respond.js',
-        'lib/html5shiv.js',
+  'js'      => array(
+    'jquery/ui/jquery-ui.min.js',
+
+    'lib/jstorage.min.js', //used for favorites - there is still some amount of JS code inline of the page -> Todo: Refactoring in upcoming Sprints
+    //wird in swissbib/AdvancedSearch.js verwendet
+    'lib/handlebars.js',
+    'lib/respond.js',
+    'lib/html5shiv.js',
 
     //https://github.com/jquery/jquery-migrate/ brauchen wir das?
-        'jquery/plugin/jquery-migrate-1.2.1.js',
-        //brauchen wir das?
-    //    'jquery/plugin/jquery.easing.js',
-    //    'jquery/plugin/jquery.debug.js',
-        //wurde im alten Node Layout verwendet
-    //    'jquery/plugin/colorbox/jquery.colorbox.js', //popup dialog solution
-        'jquery/plugin/jquery.cookie.js',
-        'jquery/plugin/jquery.spritely.js', // sprite animation, e.g. for ajax spinner
-      //  'jquery/plugin/jquery.validate.min.js',
-      //  'jquery/plugin/jquery.hoverintent.js',
-        'jquery/plugin/loadmask/jquery.loadmask.js',
-      //  'jquery/plugin/jquery.form.min.js',
-       //  von Nose erstellt  - noch benötigt?
-      //  'swissbib-jq-plugins/hint.js',
-        //menunav qird von swissbib.js verwendet -
-        //ob wir swissbib.js so noch benötigen ist für mich eine Frage. z.B. AdvancedSearch verwendet zumindest den Namespace
-        //swissbib der in swissbib.js definiert ist. Diese Abhängigkeiten und was genau verwendet wird muss man sich anschauen
-      //  'swissbib-jq-plugins/menunav.js',
-      //  'swissbib-jq-plugins/info.js',
-      //  'swissbib-jq-plugins/info.rollover.js',
-      //  'swissbib-jq-plugins/toggler.js',
-      //  'swissbib-jq-plugins/checker.js',
-      //  'swissbib-jq-plugins/dropdown.js',
-      //  'swissbib-jq-plugins/tabbed.js',
-      //  'swissbib-jq-plugins/enhancedsearch.js',
-        //scheint durch eine A
-      //  'swissbib-jq-plugins/extended.ui.autocomplete.js',
-        '../themes/bootstrap3/js/vendor/jsTree/jstree.min.js',
+    'jquery/plugin/jquery-migrate-1.2.1.js',
+    'jquery/plugin/jquery.base64.js',
+    //brauchen wir das?
+    //'jquery/plugin/jquery.easing.js',
+    //'jquery/plugin/jquery.debug.js',
+    //wurde im alten Node Layout verwendet
+    //'jquery/plugin/colorbox/jquery.colorbox.js', //popup dialog solution
+    'jquery/plugin/jquery.cookie.js',
+    'jquery/plugin/jquery.spritely.js', // sprite animation, e.g. for ajax spinner
+    //'jquery/plugin/jquery.validate.min.js',
+    //'jquery/plugin/jquery.hoverintent.js',
+    'jquery/plugin/loadmask/jquery.loadmask.js',
+    //'jquery/plugin/jquery.form.min.js',
+    //von Nose erstellt  - noch benötigt?
+    //'swissbib-jq-plugins/hint.js',
+    //menunav qird von swissbib.js verwendet -
+    //ob wir swissbib.js so noch benötigen ist für mich eine Frage. z.B. AdvancedSearch verwendet zumindest den Namespace
+    //swissbib der in swissbib.js definiert ist. Diese Abhängigkeiten und was genau verwendet wird muss man sich anschauen
+    //'swissbib-jq-plugins/menunav.js',
+    //'swissbib-jq-plugins/info.js',
+    //'swissbib-jq-plugins/info.rollover.js',
+    //'swissbib-jq-plugins/toggler.js',
+    //'swissbib-jq-plugins/checker.js',
+    //'swissbib-jq-plugins/dropdown.js',
+    //'swissbib-jq-plugins/tabbed.js',
+    //'swissbib-jq-plugins/enhancedsearch.js',
+    //scheint durch eine A
+    //'swissbib-jq-plugins/extended.ui.autocomplete.js',
+    '../themes/bootstrap3/js/vendor/jsTree/jstree.min.js',
 
-        'swissbib/swissbib.js',
+    'swissbib/swissbib.js',
 
-        'swissbib/AdvancedSearch.js',
-        'swissbib/Holdings.js',
-        'swissbib/HoldingFavorites.js',
-        'swissbib/FavoriteInstitutions.js',
-        'swissbib/Account.js',
-        'swissbib/Settings.js',
-        'swissbib/OffCanvas.js',
+    'swissbib/AdvancedSearch.js',
+    'swissbib/Holdings.js',
+    'swissbib/HoldingFavorites.js',
+    'swissbib/FavoriteInstitutions.js',
+    'swissbib/Account.js',
+    'swissbib/Settings.js',
+    'swissbib/OffCanvas.js',
+  ),
+
+  'helpers' => array(
+    'factories'  => array(
+      'record'                    => 'Swissbib\View\Helper\Swissbib\Factory::getRecordHelper',
+      'flashmessages'             => 'Swissbib\View\Helper\Swissbib\Factory::getFlashMessages',
+      'citation'                  => 'Swissbib\View\Helper\Swissbib\Factory::getCitation',
+      'recordlink'                => 'Swissbib\View\Helper\Swissbib\Factory::getRecordLink',
+      'getextendedlastsearchlink' => 'Swissbib\View\Helper\Swissbib\Factory::getExtendedLastSearchLink',
+      'auth'                      => 'Swissbib\View\Helper\Swissbib\Factory::getAuth',
+      'layoutClass'               => 'Swissbib\View\Helper\Swissbib\Factory::getLayoutClass',
+      'searchtabs'                => 'Swissbib\View\Helper\Swissbib\Factory::getSearchTabs',
+      'searchParams'              => 'Swissbib\View\Helper\Swissbib\Factory::getSearchParams',
+      'searchOptions'             => 'Swissbib\View\Helper\Swissbib\Factory::getSearchOptions',
+      'searchBox'                 => 'Swissbib\View\Helper\Swissbib\Factory::getSearchBox',
+      'includeTemplate'           => 'Swissbib\View\Helper\Swissbib\Factory::getIncludeTemplate',
+      'translateFacets'           => 'Swissbib\View\Helper\Swissbib\Factory::getFacetTranslator'
     ),
-
-    'helpers' => array(
-        'factories' => array(
-            'record' => 'Swissbib\View\Helper\Swissbib\Factory::getRecordHelper',
-            'flashmessages' => 'Swissbib\View\Helper\Swissbib\Factory::getFlashMessages',
-            'citation' => 'Swissbib\View\Helper\Swissbib\Factory::getCitation',
-            'recordlink' => 'Swissbib\View\Helper\Swissbib\Factory::getRecordLink',
-            'getextendedlastsearchlink' => 'Swissbib\View\Helper\Swissbib\Factory::getExtendedLastSearchLink',
-            'auth' => 'Swissbib\View\Helper\Swissbib\Factory::getAuth',
-            'layoutClass' => 'Swissbib\View\Helper\Swissbib\Factory::getLayoutClass',
-            'searchtabs' => 'Swissbib\View\Helper\Swissbib\Factory::getSearchTabs',
-            'searchParams' => 'Swissbib\View\Helper\Swissbib\Factory::getSearchParams',
-            'searchOptions' => 'Swissbib\View\Helper\Swissbib\Factory::getSearchOptions',
-            'searchBox' => 'Swissbib\View\Helper\Swissbib\Factory::getSearchBox',
-            'includeTemplate' => 'Swissbib\View\Helper\Swissbib\Factory::getIncludeTemplate',
-            'translateFacets' => 'Swissbib\View\Helper\Swissbib\Factory::getFacetTranslator'
-        ),
-        'invokables' => array(
-            'translate' => 'Swissbib\VuFind\View\Helper\Root\Translate',
-        )
+    'invokables' => array(
+      'translate' => 'Swissbib\VuFind\View\Helper\Root\Translate',
     )
+  )
 );
