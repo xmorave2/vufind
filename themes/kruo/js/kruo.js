3479958dc795424b274e70a061beac002965ee8d
Josef Moravec <josef.moravec@gmail.com>
2017-10-31 13:04:26 +0100

    Add kruo.cz theme

---
+++ themes/kruo/js/kruo.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    $("i.fa-check-square-o").parent(".checkbox-filter").css({
+        "background-color": "#ff6d00",
+        "border-color": "#ff6d00",
+        "color": "#fff"
+    });
+});
---
+++ themes/kruo/theme.config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'extends' => 'sandal',
+    'css' => [
+        'kruo.css',
+    ],
+    'js' => [
+        'kruo.js',
+    ],
+);
