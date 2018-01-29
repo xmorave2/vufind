e6a325e4c95c59e3e13e0d17064064d041f88a81
Josef Moravec <josef.moravec@gmail.com>
2018-01-29 09:08:13 +0100

    Enhance idex script

--- updateSolr.sh
+++ updateSolr.sh
@@ -5,6 +5,12 @@ export VUFIND_LOCAL_DIR="/usr/local/vufind/local"
 
 cd $VUFIND_HOME
 
-php public/index.php harvest harvest_oai >> harvest.log 2>&1
+php public/index.php harvest harvest_oai MK-CHOCEN >> harvest.log 2>&1
 
 harvest/batch-import-marc.sh MK-CHOCEN >> harvest.log 2>&1
+
+export VUFIND_LOCAL_DIR="/usr/local/vufind/local/chocen-region"
+
+#php public/index.php harvest harvest_oai REGION-CHOCEN >> harvest.log 2>&1
+
+harvest/batch-import-marc.sh REGION-CHOCEN >> harvest.log 2>&1
