e517bf8ff52ee6bef92e7142e6895047714a0093
Josef Moravec <joser.moravec@gmail.com>
2019-03-05 07:33:54 +0100

    Update import rules for letohrad

--- updateSolr.sh
+++ updateSolr.sh
@@ -5,19 +5,13 @@ export VUFIND_LOCAL_DIR="/usr/local/vufind/local"
 
 cd $VUFIND_HOME
 
-php public/index.php harvest harvest_oai MK-CHOCEN >> harvest-mk-chocen.log 2>&1
-harvest/batch-import-marc.sh -r MK-CHOCEN >> harvest-mk-chocen.log 2>&1
-harvest/batch-delete.sh MK-CHOCEN
+php public/index.php harvest harvest_oai MK-LETOHRAD >> harvest-mk-letohrad.log 2>&1
+harvest/batch-import-marc.sh -r MK-LETOHRAD >> harvest-mk-letohrad.log 2>&1
+harvest/batch-delete.sh MK-LETOHRAD
 
-export VUFIND_LOCAL_DIR="/usr/local/vufind/local/chocen-region"
-
-php public/index.php harvest harvest_oai REGION-CHOCEN >> harvest-region-chocen.log 2>&1
-harvest/batch-import-marc.sh -r REGION-CHOCEN >> harvest-region-chocen.log 2>&1
-harvest/batch-delete.sh REGION-CHOCEN
-
-export VUFIND_LOCAL_DIR="/usr/local/vufind/local/mkp"
-php public/index.php harvest harvest_oai MKP >> harvest-mkp.log 2>&1
-harvest/batch-import-marc.sh -r MKP >> harvest-mkp.log 2>&1
-harvest/batch-delete.sh MKP
+#export VUFIND_LOCAL_DIR="/usr/local/vufind/local/mkp"
+#php public/index.php harvest harvest_oai MKP >> harvest-mkp.log 2>&1
+#harvest/batch-import-marc.sh -r MKP >> harvest-mkp.log 2>&1
+#harvest/batch-delete.sh MKP
 
 
